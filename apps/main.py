53b9b3cf4da45fc70be3930dc7b20ae34e06a305
victor_nuzhnyi <nuzhniyva@gmail.com>
2024-05-07 14:50:58 +0300

    master added exception handling to main.py

--- apps/main.py
+++ apps/main.py
@@ -1,8 +1,17 @@
 """Main FastAPI module."""
 
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import ValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
+from apps.common.exceptions import BackendError
+from apps.common.exceptions_handlers import (
+    backend_error_handler,
+    http_exception_handler,
+    validation_exception_handler,
+)
 from settings import Settings
 from tags_metadata import metadata
 
@@ -14,4 +23,18 @@ app.add_middleware(
     allow_credentials=Settings.CORS_ALLOW_CREDENTIALS,
     allow_methods=Settings.CORS_ALLOW_METHODS,
     allow_headers=Settings.CORS_ALLOW_HEADERS,
+)
+
+app.add_exception_handler(BackendError, backend_error_handler)  # type: ignore
+app.add_exception_handler(
+    StarletteHTTPException,
+    http_exception_handler,  # type: ignore
+)
+app.add_exception_handler(
+    RequestValidationError,
+    validation_exception_handler,  # type: ignore
+)
+app.add_exception_handler(
+    ValidationError,
+    validation_exception_handler,  # type: ignore
 )
