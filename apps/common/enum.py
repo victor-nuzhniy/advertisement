60766539aed4d2d9d892c7b28bd47d47bc945df6
victor_nuzhnyi <nuzhniyva@gmail.com>
2024-05-07 14:40:43 +0300

    master created common enum.py

---
+++ apps/common/enum.py
@@ -0,0 +1,11 @@
+"""App enums."""
+
+from enum import Enum
+
+
+class JSENDStatus(str, Enum):
+    """Enum based class to set type of JSEND statuses."""
+
+    SUCCESS = 'success'
+    FAIL = 'fail'
+    ERROR = 'error'
