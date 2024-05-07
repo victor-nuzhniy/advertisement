ec53120a330271f482a07b481a739a51842f8d69
victor_nuzhnyi <nuzhniyva@gmail.com>
2024-05-07 16:12:53 +0300

    master added common orm_services.py

---
+++ apps/common/orm_services.py
@@ -0,0 +1,42 @@
+"""Project SQLAlchemy orm services."""
+
+from typing import Any
+
+from sqlalchemy import Delete
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.selectable import TypedReturnsRows
+from typing_extensions import Sequence
+
+from apps.common.common_types import ModelType
+
+
+class StatementExecutor:
+    """Async CRUD operations."""
+
+    async def execute_return_statement(
+        self,
+        session: AsyncSession,
+        statement: TypedReturnsRows[Any],
+        commit: bool = False,
+        many: bool = False,
+    ) -> ModelType | Sequence[ModelType] | None:
+        """Execute statement with returning data."""
+        alchemy_result: Result[Any] = await session.execute(statement)
+        if commit:
+            await session.commit()
+        if many:
+            return alchemy_result.scalars().all()
+        return alchemy_result.scalar_one_or_none()
+
+    async def execute_delete_statement(
+        self,
+        session: AsyncSession,
+        statement: Delete,
+    ) -> None:
+        """Execute delete statement."""
+        await session.execute(statement)
+        await session.commit()
+
+
+statement_executor = StatementExecutor()
