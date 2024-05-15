65b8ae9574b70f92cc67ca24db282aeec2c1b6bb
victor_nuzhnyi <nuzhniyva@gmail.com>
2024-05-15 21:26:05 +0300

    dev_scrapy added scrapy middleware and settings

--- spider/middlewares.py
+++ spider/middlewares.py
@@ -10,10 +10,11 @@ from scrapy import signals
 
 class SpiderSpiderMiddleware:
     """
-        Not all methods need to be defined. If a method is not defined,
-        scrapy acts as if the spider middleware does not modify the
-        passed objects.
+    Not all methods need to be defined. If a method is not defined,
+    scrapy acts as if the spider middleware does not modify the
+    passed objects.
     """
+
     @classmethod
     def from_crawler(cls, crawler):
         """This method is used by Scrapy to create your spiders."""
