115b414ad7145898dc55e9137c1d406c0d5743fd
victor_nuzhnyi <nuzhniyva@gmail.com>
2024-05-16 18:55:36 +0300

    dev_scrapy added crontab settings to celery app

--- celery_app/worker.py
+++ celery_app/worker.py
@@ -1,6 +1,9 @@
 """Celery worker."""
 
+from typing import Any
+
 from celery import Celery
+from celery.schedules import crontab
 
 from settings import Settings
 from spider.project_utilities.save_utilities import remove_old_adv
@@ -11,8 +14,21 @@ celery.conf.broker_url = Settings.CELERY_BROKER_REDIS_URL
 celery.conf.result_backend = Settings.CELERY_RESULT_BACKEND
 
 
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs: Any) -> None:  # type: ignore
+    """Set periodic tasks."""
+    sender.add_periodic_task(
+        crontab(hour=Settings.CLEAN_TIME),
+        clean_db.s(),
+    )
+    sender.add_periodic_task(
+        crontab(hour=Settings.SCRAP_TIME),
+        scrap.s(),
+    )
+
+
 @celery.task(name='scrap')
-def create_task() -> None:
+def scrap() -> None:
     """Run crawler task."""
     run_crawler()
 
