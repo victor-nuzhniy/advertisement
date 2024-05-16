d0b1df7044e6c8181ce333c509ba23fc71455791
victor_nuzhnyi <nuzhniyva@gmail.com>
2024-05-16 18:08:02 +0300

    dev_scrapy added tasks to celery worker

--- celery_app/worker.py
+++ celery_app/worker.py
@@ -3,7 +3,21 @@
 from celery import Celery
 
 from settings import Settings
+from spider.project_utilities.save_utilities import remove_old_adv
+from spider.scripts import run_crawler
 
 celery = Celery(__name__)
 celery.conf.broker_url = Settings.CELERY_BROKER_REDIS_URL
 celery.conf.result_backend = Settings.CELERY_RESULT_BACKEND
+
+
+@celery.task(name='scrap')
+def create_task() -> None:
+    """Run crawler task."""
+    run_crawler()
+
+
+@celery.task(name='clean_db')
+def clean_db() -> None:
+    """Clean db from old advertisements task."""
+    remove_old_adv()
