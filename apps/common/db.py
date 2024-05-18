244c6341699c7d4ebcf06604137ad78cab2e61af
victor_nuzhnyi <nuzhniyva@gmail.com>
2024-05-18 15:13:03 +0300

    test changes in db.py - changed import declarative_base

--- apps/common/db.py
+++ apps/common/db.py
@@ -2,8 +2,7 @@
 
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
 from settings import Settings
 
