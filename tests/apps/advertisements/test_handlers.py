7a32e2f7984322ef77298cd93dda61eeb01f33bd
victor_nuzhnyi <nuzhniyva@gmail.com>
2024-05-20 11:57:55 +0300

    test added TestBulkCreateAdv class

--- tests/apps/advertisements/test_handlers.py
+++ tests/apps/advertisements/test_handlers.py
@@ -2,13 +2,19 @@
 
 from typing import Sequence, Sized
 
+import factory
 from faker import Faker
 from fastapi import Request
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from apps.advertisements.handlers import adv_handlers
 from apps.advertisements.models import Advertisement
-from apps.advertisements.schemas import AdvNameModelQuerySchema, AdvPeriodQuerySchema
+from apps.advertisements.schemas import (
+    AdvInList,
+    AdvNameModelQuerySchema,
+    AdvPeriodQuerySchema,
+    CreateAdvIn,
+)
 from tests.apps.advertisements.factories import AdvertisementFactory
 
 
@@ -56,6 +62,10 @@ class TestAdvHandlersGetAdvPeriodList:
                 elem.id == actual_result[index + len(already_in_db)].id  # type: ignore
             )
 
+
+class TestGetNameModelStat:
+    """Class for testing get_name_model_stat handler."""
+
     async def test_get_name_model_stat(
         self,
         faker: Faker,
@@ -95,3 +105,34 @@ class TestAdvHandlersGetAdvPeriodList:
         assert actual_result['num_day'] == number
         assert actual_result['num_week'] == 2 * number
         assert actual_result['num_month'] == 3 * number
+
+
+class TestBulkCreateAdv:
+    """Class for testing bulk_create_adv handler."""
+
+    async def test_bulk_create_adv(
+        self,
+        faker: Faker,
+        db_session: AsyncSession,
+    ) -> None:
+        """Test bulk_create_adv handler."""
+        number: int = faker.random_int(min=3, max=5)
+        expected_result: list[CreateAdvIn | None] = []
+        for _ in range(number):
+            model_dict = factory.build(dict, FACTORY_CLASS=AdvertisementFactory)
+            model_dict.pop('created_at')
+            expected_result.append(CreateAdvIn(**model_dict))
+        await adv_handlers.bulk_create_adv(
+            Request({'type': 'http'}),
+            db_session,
+            AdvInList(item_list=expected_result),
+        )
+        saved_advs = await adv_handlers.get_adv_period_list(
+            Request({'type': 'http'}),
+            db_session,
+            AdvPeriodQuerySchema(),
+        )
+        assert isinstance(saved_advs, Sequence)
+        for index, elem in enumerate(saved_advs[-number:]):
+            for attr in ('name', 'price', 'url', 'model', 'adv_date'):
+                assert getattr(expected_result[index], attr) == getattr(elem, attr)
