1314d7a99ad4c9cbe34683c400c3ebc3dbc96658
victor_nuzhnyi <nuzhniyva@gmail.com>
2024-06-03 22:10:24 +0300

    test added refresh auth router

--- apps/authorization/routers.py
+++ apps/authorization/routers.py
@@ -1,12 +1,12 @@
 """Authorization apps routers."""
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Header, Request
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing_extensions import Annotated
 
 from apps.authorization.handlers import authorization_handlers
-from apps.authorization.schemas import AuthOut
+from apps.authorization.schemas import AuthOut, RefreshOut
 from apps.common.dependencies import get_async_session
 
 authorization_router = APIRouter()
@@ -42,3 +42,28 @@ async def login(
         form_data=form_data,
         session=session,
     )
+
+
+@authorization_router.post(
+    '/refresh-token/',
+    name='refresh',
+    response_model=RefreshOut,
+    summary='Refresh access token',
+    responses={},
+    tags=['Authorization application'],
+)
+def refresh(
+    request: Request,
+    refresh_token: Annotated[str, Header()],
+) -> RefreshOut:
+    """
+    **Refresh access token**.
+
+    **Headers:**
+    - **refresh_token:** token for refresh access token
+
+    **Return:**
+    - **access_token:** token for access to endpoints
+    """
+    token = authorization_handlers.refresh(request=request, refresh_token=refresh_token)
+    return RefreshOut(access_token=token)
